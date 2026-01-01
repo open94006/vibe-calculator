5489d78f7e096e1119d173974a0969d15e973e53
open94006 <open94006880103@gmail.com>
2026-01-01 14:57:03 +0800

    add env & dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import productRoute from './routes/product.route';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
